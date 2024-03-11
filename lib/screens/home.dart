fe074e940d3293b3b17686a597cf95b98cb85275
Alvin Pratama <palveeen22@gmail.com>
2024-03-11 15:18:35 +0700

    style: added style searchBox for search input

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -16,19 +16,32 @@ class _HomeState extends State<Home> {
       body: Container(
         padding: EdgeInsets.symmetric(horizontal: 15),
         child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: TextField(),
-            ),
-          ],
+          children: [searchBox()],
         ),
       ),
     );
   }
+}
+
+@override
+Widget searchBox() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 15),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(20),
+    ),
+    child: TextField(
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(0),
+        prefixIcon: Icon(Icons.search, color: tdBlack, size: 20),
+        prefixIconConstraints: BoxConstraints(maxHeight: 20, minWidth: 25),
+        border: InputBorder.none,
+        hintText: "Search",
+        hintStyle: TextStyle(color: tdGrey),
+      ),
+    ),
+  );
 }
 
 AppBar _buildAppBar() {
